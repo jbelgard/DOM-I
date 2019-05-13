7fecfef973ed2a4711416e999de12d17a9ee0e60
Jason Belgard <belgard.jason@gmail.com>
2019-05-13 14:41:44 -0500

    MVP met

--- js/index.js
+++ js/index.js
@@ -41,37 +41,57 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-const anchors = document.querySelectorAll('a')
-for(let i=0; i<anchors.length; i++) {
-  anchors[i].innerHTML = Object.values(siteContent['nav'])[i]
-}
+let Nav = document.querySelector("nav");
+let navAnchors = document.querySelectorAll('a');
+let mainHeader = document.querySelector('h1');
+let headerButton = document.querySelector('button');
+let headerImg = document.getElementById('cta-img');
+let H4 = document.querySelectorAll("h4");
+let paragraphs = document.querySelectorAll("p");
+let middleImg = document.getElementById("middle-img");
 
-const queryhtml = (query, index) => {
-  return document.querySelector(query).innerHTML = siteContent[index][query]
-}
-
-queryhtml('h1', 'cta');
-queryhtml('button', 'cta');
-
-const h4main = document.querySelector(".main-content").querySelectorAll('h4');
-const p = document.querySelector(".main-content").querySelectorAll("p");
-const objMain = siteContent["main-content"];
-const mainImg = siteContent["main-content"]["middle-img-src"];
-delete siteContent["main-content"]["middle-img-src"];
-
-for(let i=0; i<h4main.length; i++) {
-  for(let n=0; n<Object.values(objMain).length; n++) {
-    n%2 ==0 ? h4main[i].innerHTML = Object.values(objMain)[n] : p[i].innerHTML = Object.values(objeMain)[n]
-  }
-}
-
-document.getElementById('middle-img').setAttribute('src', mainImg);
-
-const contact = document.querySelector('.contact')
-contact.querySelector("h4").innerHTML = siteContent["contact"]["contact-h4"];
-
-contact.querySelectorAll("p").forEach((a,b) => {
-  return a.innerHTML = Object.values(siteContent["contact"])[b + 1]
+let counter = 1;
+navAnchors.forEach(function(element) {
+  element.textContent = siteContent['nav'][`nav-item-${counter}`]
+  element.style.color = 'green';
+  counter++;
 })
 
-document.querySelectorAll("p")[document.querySelectorAll("p").length - 1].innerHTML = siteContent["footer"]["copyright"];
+const newAnchor1 = document.createElement("a");
+const newAnchor2 = document.createElement("a");
+
+newAnchor1.textContent = "Hello";
+newAnchor2.textContent = "World";
+
+newAnchor1.style.color = "green";
+newAnchor2.style.color = "green";
+
+Nav.appendChild(newAnchor1);
+Nav.appendChild(newAnchor2);
+
+mainHeader.textContent = siteContent["cta"]["h1"];
+
+headerImg.setAttribute('src', siteContent["cta"]["img-src"]);
+headerButton.textContent = siteContent["cta"]["button"];
+
+H4[0].textContent = siteContent["main-content"]["features-h4"];
+H4[1].textContent = siteContent["main-content"]["about-h4"];
+H4[2].textContent = siteContent["main-content"]["services-h4"];
+H4[3].textContent = siteContent["main-content"]["product-h4"];
+H4[4].textContent = siteContent["main-content"]["vision-h4"];
+
+paragraphs[0].textContent = siteContent["main-content"]["features-content"];
+paragraphs[1].textContent = siteContent["main-content"]["about-content"];
+paragraphs[2].textContent = siteContent["main-content"]["services-content"];
+paragraphs[3].textContent = siteContent["main-content"]["product-content"];
+paragraphs[4].textContent = siteContent["main-content"]["vision-content"];
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+H4[5].textContent = siteContent["contact"]["contact-h4"];
+paragraphs[5].textContent = siteContent["contact"]["address"];
+paragraphs[6].textContent = siteContent["contact"]["phone"];
+paragraphs[7].textContent = siteContent["contact"]["email"];
+
+paragraphs[8].textContent = siteContent["footer"]["copyright"];
+
+console.log(paragraphs);
